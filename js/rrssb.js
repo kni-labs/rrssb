ecbf9d5c8a094e46484ea2b0cf188dce451e82c6
Joshua Tuscan <hello@joshuatuscan.com>
2014-02-18 20:20:21 -0800

    wait longer to switch to icon view

--- js/rrssb.js
+++ js/rrssb.js
@@ -24,9 +24,9 @@ var makeExtremityBtns = function() {
 		$('.rrssb-buttons').removeClass('large-format');
 	}
 
-	if (containerWidth < 240) {
+	if (containerWidth < 210) {
 		$('.rrssb-buttons').removeClass('small-format').addClass('tiny-format');
-	} else if (containerWidth > 239) {
+	} else if (containerWidth > 209) {
 		$('.rrssb-buttons').removeClass('tiny-format');
 	}
 };
